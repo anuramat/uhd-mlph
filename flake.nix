20ceebd50912991ef4a7baa080bad36a3fcb6fbc
Arsen Nuramatov <anuramat@pm.me>
2024-10-28 00:53:19 +0200

    feat(deps): flake, direnv

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            cudaSupport = true;
+            cudnnSupoprt = true;
+          };
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            (pkgs.python3.withPackages (python-pkgs:
+              with python-pkgs; [
+                jupyter
+                jupytext
+                numpy
+                scipy
+                scikit-learn
+                matplotlib
+                tqdm
+                pytest
+                # torch
+                # torchvision
+                # pytorch-lightning # import as `pytorch_lightning`
+                # tensorboard
+              ]))
+            pkgs.cudaPackages.cudatoolkit
+          ];
+        };
+      }
+    );
+}
